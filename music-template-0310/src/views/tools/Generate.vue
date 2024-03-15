dee71958429f0404a5686220cdd9cf4d2065a610
Leeasy <793556890@qq.com>
2024-03-15 18:44:48 +0800

    修改

--- music-template-0310/src/views/tools/Generate.vue
+++ music-template-0310/src/views/tools/Generate.vue
@@ -1,15 +1,21 @@
 <template>
-    
   <div class="page-container">
+    <iframe src="http://127.0.0.1:5500/index.html"></iframe>
   </div>
 </template>
 
 <style>
 .page-container {
-  background: linear-gradient(to right, #7E3D00, #ffd1a7, #7E3D00); /* 替换 #color1, #color2, #color3 为实际颜色代码 */
+  background: linear-gradient(to right, #7E3D00, #ffd1a7, #7E3D00);
   background-attachment: fixed;
   background-position: center center;
-  position: relative; /* 设置相对定位，以便按钮可以使用绝对定位 */
-  height: 100vh; /* 设置为视口高度，或根据需要调整 */
+  position: relative;
+  height: 100vh;
+}
+
+.page-container iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
 }
 </style>
